662d4159c768ec5ac1a7a378cf9563dc9f00f984
JamesGuenther <lordcheetah@comcast.net>
2017-06-09 22:47:36 -0400

    Latest Update

---
+++ Assets/Scripts/SocialAchievements.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SocialAchievements : MonoBehaviour {
+	public Canvas canvas;
+	public Text text;
+
+	private bool helpAchievement = false;
+	private bool scaredAchievement = false;
+	private bool mehAchievement = false;
+
+	void Start () {
+		canvas.gameObject.SetActive(false);
+	}
+
+	public void UnlockHelpAchievement()
+	{
+		StartCoroutine (HelpAchievement());
+	}
+
+	IEnumerator HelpAchievement()
+	{
+		Debug.Log ("Help Ach");
+		if (helpAchievement)
+			yield break;
+
+		helpAchievement = true;
+
+		text.text = "Help Achievement\n\nYou helped the little spheres!";
+		canvas.gameObject.SetActive(true);
+		yield return new WaitForSeconds(5);
+		canvas.gameObject.SetActive(false);
+	}
+
+	public void UnlockScaredAchievement()
+	{
+		StartCoroutine (ScaredAchievement());
+	}
+
+	IEnumerator ScaredAchievement()
+	{
+		Debug.Log ("Scared Ach");
+		if (scaredAchievement)
+			yield break;
+
+		scaredAchievement = true;
+
+		text.text = "Scared Achievement\n\nYou scared the little spheres. What's wrong with you? Try again.";
+		canvas.gameObject.SetActive(true);
+		yield return new WaitForSeconds(5);
+		canvas.gameObject.SetActive(false);
+	}
+
+	public void UnlockMehAchievement()
+	{
+		StartCoroutine (MehAchievement());
+	}
+
+	IEnumerator MehAchievement()
+	{
+
+		Debug.Log ("Meh Ach");
+		if (mehAchievement)
+			yield break;
+
+		mehAchievement = true;
+
+		text.text = "Meh Achievement\n\nYou ignored the little spheres. What are you doing? Go back and help them!";
+		canvas.gameObject.SetActive(true);
+		yield return new WaitForSeconds(5);
+		canvas.gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/SphereTalk.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereTalk : MonoBehaviour {
+	private Animator anim;
+	private float talkTime = 3.0f;
+
+	void Start()
+	{
+		anim = this.GetComponent<Animator> ();
+
+		Talk ();
+		Neutral_Fear ();
+	}
+
+	void Update()
+	{
+		talkTime -= Time.deltaTime;
+		if (talkTime <= 0)
+		{
+			Fear_Neutral ();
+			StopTalking ();
+		}
+	}
+
+	void Talk()
+	{
+		anim.SetTrigger ("Talk");
+	}
+
+	void StopTalking()
+	{
+		anim.SetTrigger ("StopTalking");
+	}
+
+	void Neutral_Angry()
+	{
+		anim.SetTrigger ("Neutral_Angry");
+	}
+
+	void Angry_Neutral()
+	{
+		anim.SetTrigger ("Angry_Neutral");
+	}
+
+	void Neutral_Fear()
+	{
+		anim.SetTrigger ("Neutral_Fear");
+		anim.SetTrigger ("Shake");
+	}
+
+	void Fear_Neutral()
+	{
+		anim.SetTrigger ("Fear_Neutral");
+		anim.SetTrigger ("StopShaking");
+	}
+}
