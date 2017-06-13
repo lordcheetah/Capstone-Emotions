6afbba25a50be138421ec65eed6fa326983f8ece
James Guenther <lordcheetah@users.noreply.github.com>
2017-06-13 18:37:04 -0400

    Feat: Cut Scene Manager, Animation, Achievements

--- Assets/Scripts/SphereTalk.cs
+++ Assets/Scripts/SphereTalk.cs
@@ -2,55 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SphereTalk : MonoBehaviour {
+public class SphereTalk : MonoBehaviour
+{
 	private Animator anim;
-	private float talkTime = 3.0f;
 
 	void Start()
 	{
 		anim = this.GetComponent<Animator> ();
-
-		Talk ();
-		Neutral_Fear ();
 	}
 
-	void Update()
-	{
-		talkTime -= Time.deltaTime;
-		if (talkTime <= 0)
-		{
-			Fear_Neutral ();
-			StopTalking ();
-		}
-	}
-
-	void Talk()
+	public void Talk()
 	{
 		anim.SetTrigger ("Talk");
 	}
 
-	void StopTalking()
+	public void StopTalking()
 	{
 		anim.SetTrigger ("StopTalking");
 	}
 
-	void Neutral_Angry()
+	public void Neutral_Angry()
 	{
 		anim.SetTrigger ("Neutral_Angry");
 	}
 
-	void Angry_Neutral()
+	public void Angry_Neutral()
 	{
 		anim.SetTrigger ("Angry_Neutral");
 	}
 
-	void Neutral_Fear()
+	public void Neutral_Fear()
 	{
 		anim.SetTrigger ("Neutral_Fear");
 		anim.SetTrigger ("Shake");
 	}
 
-	void Fear_Neutral()
+	public void Fear_Neutral()
 	{
 		anim.SetTrigger ("Fear_Neutral");
 		anim.SetTrigger ("StopShaking");
